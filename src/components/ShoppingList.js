be0b6f918c926fcc0a51dbeee934e692b5ad6fdf
Colombianito66 <colombianito66@gmail.com>
2018-09-27 13:33:21 -0400

    Shopping List component scaffold up and running

---
+++ src/components/ShoppingList.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react';
+
+class ShoppingList extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+
+    }
+  }
+  render() {
+    return (
+      <div>
+        <h1 className="title">Florida Blue - Shopping List</h1>
+      </div>
+    )
+  }
+}
+
+export default ShoppingList;
